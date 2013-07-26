3993d956d9273c679edd3ce311e3333902dd7203
biffbyrd <jeffreydbyrd@gmail.com>
2013-07-26 07:45:21 -0400

    added a simple test for getCommand

--- test/game/PlayerModuleSpec.scala
+++ test/game/PlayerModuleSpec.scala
@@ -1,11 +1,35 @@
 package game
 
 import org.specs2.mutable.Specification
+import play.api.libs.json.JsValue
+import play.api.libs.json.Json
 
-trait PlayerModuleSpec 
-	extends PlayerModule
-	with Specification {
-  
-  
-  
+class PlayerModuleSpec
+    extends PlayerModule
+    with Specification {
+
+  "getCommand(JsValue)" should {
+    val kd: JsValue = Json.obj(
+      "type" -> "keydown",
+      "data" -> 65
+    )
+
+    val ku: JsValue = Json.obj(
+      "type" -> "keyup",
+      "data" -> 65
+    )
+
+    val clk = Json.obj(
+      "type" -> "click",
+      "data" -> Json.obj(
+        "x" -> 42,
+        "y" -> 5
+      )
+    )
+
+    "return KeyUp( 65 ) when json = ku" in {
+      getCommand( ku ) === KeyUp( 65 )
+    }
+  }
+
 }
