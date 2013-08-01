3e12538d1e15be800be83729f6f872fe6c8caf00
biffbyrd <jeffreydbyrd@gmail.com>
2013-07-31 22:32:03 -0400

    playing around with akka testkit

--- project/Build.scala
+++ project/Build.scala
@@ -4,15 +4,14 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "scalatree"
-  val appVersion      = "1.0-SNAPSHOT"
+  val appName = "scalatree"
+  val appVersion = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    // Add your project dependencies here,
+    "com.typesafe.akka" %% "akka-testkit" % "2.3-SNAPSHOT"
   )
 
-
-  val main = play.Project(appName, appVersion, appDependencies).settings(
+  val main = play.Project( appName, appVersion, appDependencies ).settings( 
     // Add your own project settings here      
   )
 
--- test/game/PlayerModuleSpec.scala
+++ test/game/PlayerModuleSpec.scala
@@ -1,34 +1,29 @@
 package game
 
 import org.specs2.mutable.Specification
+
+import akka.testkit.TestActorRef
+import play.api.Play.current
+import play.api.libs.concurrent.Akka
 import play.api.libs.json.JsValue
 import play.api.libs.json.Json
+import play.api.libs.json.Json.toJsFieldJsValueWrapper
+
 
 class PlayerModuleSpec
     extends PlayerModule
     with Specification {
   
+  implicit val sys = Akka.system
+  val actorRef = TestActorRef(new Player("bleh"))
+  val actor = actorRef.underlyingActor
   
-  
-  
-
   "getCommand(JsValue)" should {
-    val kd: JsValue = Json.obj(
-      "type" -> "keydown",
-      "data" -> 65
-    )
-
-    val ku: JsValue = Json.obj(
-      "type" -> "keyup",
-      "data" -> 65
-    )
-
+    val kd: JsValue = Json.obj( "type" -> "keydown", "data" -> 65 )
+    val ku: JsValue = Json.obj( "type" -> "keyup", "data" -> 65 )
     val clk = Json.obj(
       "type" -> "click",
-      "data" -> Json.obj(
-        "x" -> 42,
-        "y" -> 5
-      )
+      "data" -> Json.obj( "x" -> 42, "y" -> 5 )
     )
 
     "return KeyUp( 65 ) when json = {type : 'keyup', data : 65}" in {
