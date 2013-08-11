6b739a4b725a7c07a88e6007a2f38028813c349e
biffbyrd <jeffreydbyrd@gmail.com>
2013-08-10 22:18:55 -0400

    changed application name

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName = "scalatree"
+  val appName = "doppelgamer"
   val appVersion = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
