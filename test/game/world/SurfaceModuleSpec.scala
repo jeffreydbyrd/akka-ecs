09e3cf86fd9048d5d4f30262c5a90aab3aba00b1
biffbyrd <jeffreydbyrd@gmail.com>
2013-08-24 13:37:59 -0400

    added tests for Slope.m

--- test/game/world/SurfaceModuleSpec.scala
+++ test/game/world/SurfaceModuleSpec.scala
@@ -6,7 +6,7 @@ import game.EventModule
 import game.mobile.PlayerModule
 import game.ConnectionModule
 
-trait SurfaceModuleSpec
+class SurfaceModuleSpec
     extends SurfaceModule
     with EventModule
     with RoomModule
@@ -15,6 +15,20 @@ trait SurfaceModuleSpec
     with MobileModule
     with Specification { // that's a lot of mixins
   
-  
-  
+  override val system = null
+
+  "Slope.m" should {
+    "return Double if the slope is a Slant" in {
+      Slant( 2, 2 ).m === 1.0
+    }
+
+    "return 0.0 if the slope is a Flat" in {
+      Flat.m === 0.0
+    }
+
+    "throw UndefinedSlopeException if Undefined" in {
+      Undefined.m must throwA[ UndefinedSlopeException ]
+    }
+  }
+
 }
