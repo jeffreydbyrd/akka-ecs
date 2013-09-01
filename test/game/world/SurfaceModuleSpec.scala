05653e1770fdaeefd6d065814dd2285dcfd919cd
biffbyrd <jeffreydbyrd@gmail.com>
2013-09-01 10:25:55 -0400

    wrote better tests for isLanding

--- test/game/world/SurfaceModuleSpec.scala
+++ test/game/world/SurfaceModuleSpec.scala
@@ -33,20 +33,20 @@ class SurfaceModuleSpec
   }
 
   "Floor.isLanding( Position, Movement )" should {
-    "return true when the Mobile's vector crosses a Flat Floor" in {
+    "return true when the Mobile's vector is vertical and crosses a Flat Floor" in {
       DoubleSided( Point( 0, 1 ), Point( 10, 1 ) ).isLanding( Position( 3, 4 ), Movement( 0, -2 ) ) === true
     }
 
-    "return false when Mobile's vector doesn't cross a Flat Floor" in {
+    "return false when the Mobile's vector is vertical and doesn't cross a Flat Floor" in {
       DoubleSided( Point( 0, 1 ), Point( 10, 1 ) ).isLanding( Position( 3, 10 ), Movement( 0, -2 ) ) === false
     }
 
-    "return true when Mobile's vector crosses a Slanted floor" in {
-      DoubleSided( Point( 3, 2 ), Point( 5, 4 ) ).isLanding( Position( 4, 4 ), Movement( 0, -5 ) ) === true
+    "return true when the Mobile's vector is Slanted and crosses a Slanted floor" in {
+      DoubleSided( Point( 0, 0 ), Point( 4, 4 ) ).isLanding( Position( 1, 5 ), Movement( 3, -3 ) ) === true
     }
 
-    "return false when Mobile's vector doesn't cross a Slanted floor" in {
-      DoubleSided( Point( 3, 2 ), Point( 5, 4 ) ).isLanding( Position( 4, 10 ), Movement( 0, -1 ) ) === false
+    "return false when the Mobile's vector intersects with the Surface *outside* of its X and Y bounds." in {
+      DoubleSided( Point( 0, 0 ), Point( 4, 4 ) ).isLanding( Position( 3, 8 ), Movement( 3, -3 ) ) === false
     }
   }
 
