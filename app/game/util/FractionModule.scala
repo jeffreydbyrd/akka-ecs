787696ba794f15a8f6fb306c6e44ffb511498cce
biffbyrd <jeffreydbyrd@gmail.com>
2013-09-01 20:47:30 -0400

    refactored Fraction and did away with the whole 'nested fractions' thing. It was a fun exercise but this method uses half the lines

--- app/game/util/FractionModule.scala
+++ app/game/util/FractionModule.scala
@@ -2,51 +2,21 @@ package game.util
 
 trait FractionModule {
 
-  implicit def intToFrac( i: Int ): FracInt = FracInt( i )
-  implicit def fracToInt( f: FracInt ): Int = f.i
+  implicit def intToFract( i: Int ): Fraction = Fraction( i, 1 )
 
   def gcd( a: Int, b: Int ): Int = if ( b == 0 ) a else gcd( b, a % b )
 
-  sealed trait Fraction {
-    val toDouble: Double
-    val isDefined: Boolean
-    def :/( that: Fraction ) = Frac( this, that )
-    def /( that: Fraction ) = this :/ that
-    def *[ A <: Fraction ]( that: A ): Fraction
-    def +[ A <: Fraction ]( that: A ): Fraction
+  case class Fraction( private val n: Int, private val d: Int ) {
+    val toDouble: Double = n.toDouble / d.toDouble
+    val isDefined: Boolean = d != 0
+    override def toString = s"$n / $d"
+    def reduce: Fraction = {
+      val k = gcd( n, d )
+      ( ( n / k ) :/ ( d / k ) )
+    }
+    def *( that: Fraction ): Fraction = Fraction( n * that.n, d * that.d )
+    def :/( that: Fraction ) = this * Fraction( that.d, that.n )
+    def +( that: Fraction ): Fraction = Fraction( this.n * that.d + that.n * this.d, this.d * that.d )
     def -[ A <: Fraction ]( that: A ): Fraction = this + ( -1 * that )
   }
-
-  case class Frac( private val n: Fraction, private val d: Fraction ) extends Fraction {
-    val isDefined = d != FracInt( 0 )
-    override lazy val toDouble: Double = n.toDouble / d.toDouble
-    override def toString() = s"( ${n.toString} / ${d.toString} )"
-
-    override def *[ A <: Fraction ]( that: A ) = that match {
-      case Frac( _n, _d ) ⇒ ( this.n * _n ) :/ ( this.d * _d )
-      case _: FracInt     ⇒ that * this
-    }
-
-    override def +[ A <: Fraction ]( that: A ) = that match {
-      case Frac( _n, _d ) ⇒ ( this.n * _d + _n * this.d ) :/ ( this.d * _d )
-      case _: FracInt     ⇒ that + this
-    }
-  }
-
-  case class FracInt( val i: Int ) extends Fraction {
-    val isDefined = true
-    override lazy val toDouble: Double = i.toDouble
-    override def toString() = i.toString
-
-    override def *[ A <: Fraction ]( that: A ) = that match {
-      case Frac( _n, _d ) ⇒ ( this * _n ) :/ _d
-      case FracInt( _i )  ⇒ this.i * _i
-    }
-
-    override def +[ A <: Fraction ]( that: A ) = that match {
-      case Frac( _n, _d ) ⇒ ( ( this * _d ) + _n ) :/ _d
-      case FracInt( _i )  ⇒ this.i + _i
-    }
-  }
-
 }
--- test/game/util/FractionModuleSpec.scala
+++ test/game/util/FractionModuleSpec.scala
@@ -8,24 +8,40 @@ class FractionModuleSpec
 
   "gcd should return the greatest common denominator" in {
     gcd( 8, 28 ) === 4
+    gcd( 3, 7 ) === 1
   }
 
-  ":/" should {
-    "convert (Int :/ Int) to Frac( FracInt , FracInt )" in {
-      ( 3 :/ 15 ) === Frac( FracInt( 3 ), FracInt( 15 ) )
-    }
+  "Int :/ Int should convert to Fraction( Int , Int )" in {
+    ( 3 :/ 15 ) === Fraction( 3, 15 )
+  }
 
-    "convert (Int :/ Int :/ Int) to Frac( Frac( FracInt, FracInt ), FracInt )" in {
-      ( 3 :/ 15 :/ 12 ) === Frac( Frac( 3, 15 ), 12 )
-    }
+  "(a :/ b :/ c) should convert to Fraction( a, b*c )" in {
+    ( 3 :/ 15 :/ 12 ) === Fraction( 3, 15 * 12 )
   }
 
   "Fraction.toDouble" should {
-    "convert a Frac(FracInt, FracInt) to a Double" in {
+    "convert a Fraction(a, b) to a Double" in {
       ( 3 :/ 15 ).toDouble === 0.2
     }
-    "convert a Frac( Frac( FracInt, FracInt ), FracInt ) to a Double" in {
+    "convert ( (a / b) / c ) to a Double" in {
       ( 15 :/ 3 :/ 2 ).toDouble === 2.5
+    }
+  }
+
+  "(3 :/ 4) * (4 :/ 3) should return (12 / 12)" in {
+    ( 3 :/ 4 ) * ( 4 :/ 3 ) === 12 :/ 12
+  }
+
+  "(3 :/ 4) + (4 :/ 3) should return (25 / 12)" in {
+    ( 3 :/ 4 ) + ( 4 :/ 3 ) === 25 :/ 12
+  }
+
+  "Fraction.reduce" should {
+    "turn (12 :/ 12) into 1 / 1" in {
+      ( 12 :/ 12 ).reduce === 1 :/ 1
+    }
+    "turn (8 :/ 28) into (2 / 7)" in {
+      ( 8 :/ 28 ).reduce === 2 :/ 7
     }
   }
 
