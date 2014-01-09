326f5e1c99f0b5c0015ae9de9eb41848247820d9
biffbyrd <jeffreydbyrd@gmail.com>
2014-01-09 00:19:11 -0500

    writes removeAll test

--- test/game/events/AdjustHandlerSpec.scala
+++ test/game/events/AdjustHandlerSpec.scala
@@ -4,7 +4,14 @@ import org.scalatest.FunSuiteLike
 
 class AdjustHandlerSpec extends AdjustHandler with FunSuiteLike {
   test( "removeAll should remove a subset of Adjusts from 'adjusts'" ) {
-    //      new TestEventHandler { def test( as: List[ Adjust ] ) = removeAll( adjs, as ) }
-    //        .test( List( adj0, adj2 ) ) === List( adj1, adj3 )
+    case class Test( v: Int ) extends Event
+    val adj0: Adjust = { case x ⇒ x }
+    val adj1: Adjust = { case Test( i ) ⇒ Test( 2 * i ) }
+    val adj2: Adjust = { case Test( i ) ⇒ Test( 3 * i ) }
+    val adj3: Adjust = { case Test( i ) if i % 7 == 0 ⇒ Test( 0 ) }
+
+    val adjs = List( adj0, adj1, adj2, adj3 )
+
+    removeAll( adjs, List( adj0, adj2 ) ) === List( adj1, adj3 )
   }
 }
