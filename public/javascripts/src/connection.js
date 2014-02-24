9b8045d91236f7ac4ce14480ba3cf946494dec8e
biffbyrd <jeffreydbyrd@gmail.com>
2014-02-23 22:08:00 -0500

    removes code related kinetic.js and jquery and adds pixi.js to our js libs

---
+++ public/javascripts/src/connection.js
@@ -0,0 +1,175 @@
+/*******************************************************************************
+ * The DoppelClient
+ * 
+ * Depends on kinetic.js. Extracts a `username` from the URL
+ * (http://a.b.c.d:80#username=???) and establishes a WebSocket connection with
+ * the server using `username`.
+ ******************************************************************************/
+
+/***************************************************************************
+ * Constants
+ **************************************************************************/
+
+var SCREEN_W = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+var SCREEN_H = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+var DIMENSIONS = {
+  w : SCREEN_H / 1.1,
+  h : SCREEN_H / 1.1
+};
+
+var INTERNAL_DIMENSIONS = 50;
+
+/**
+ * The server uses a room with <INTERNAL_DIMENSIONS> cells, so we need a multiplier. If the
+ * server says "move 2 units left", we need to move (2*(DIMENSIONS.h / INTERNAL_DIMENSIONS)) 
+ * pixels left.
+ */
+var K = DIMENSIONS.h / INTERNAL_DIMENSIONS;
+
+var USERNAME = function() {
+  var hash = window.location.hash;
+  var i = hash.indexOf("username=") + 9
+  return hash.substring(i);
+}();
+
+var ADDRESS = function() {
+  var href = window.location.href;
+  var addr = href.substring(7, href.indexOf("/#"));
+  return "ws://" + addr + "/test?username=" + USERNAME;
+}();
+
+/**
+ * The set of functions that the server can execute remotely. Each function
+ * takes a `data` object, which can take whatever form the function wants.
+ */
+var COMMANDS = {
+  "started": function(data) {
+    // conn.send(JSON.stringify({ type:"started" }));
+  },
+  "quit" : function(data) {
+      // conn.close();
+      // document.write("<p>" + data.message + "</p>");
+  },
+  "create" : function(data) {
+      // view.add(data);
+      // view.draw();
+      // console.log(data);
+  },
+  "move" : function(data) {
+      // view.move(data.id, data.position[0], data.position[1]);
+      // view.draw();
+      // console.log(data);
+  },
+  "delete" : function(data) {
+      // view.remove(data.id);
+  }
+};
+
+/***************************************************************************
+ * Prototypes
+ **************************************************************************/
+
+/**
+ * A Connection sends messages to and receives messages from a server.
+ * Connections expects JSON formatted strings. Connections also send ACKs
+ * back to the server to confirm that a message was received.
+ * 
+ * @param url -
+ *            a websocket URL string
+ */
+function Connection(url) {
+  var self = this;
+
+  var websocket = new WebSocket(url);
+  var count = 0;
+
+  websocket.onopen = function(evt) {
+      console.log("websocket opened");
+  };
+
+  websocket.onclose = function() {
+      console.log("websocket closed");
+  };
+
+  /** evt.data schema === {id : ???, message: { type: ???, .... } } */
+  websocket.onmessage = function(evt) {
+      console.log(evt.data);
+      var msg = JSON.parse(evt.data);
+      var id = msg.id;
+      count = id;
+      var cmd = msg.message;
+      COMMANDS[cmd.type](cmd);
+
+      var ack = JSON.stringify({
+        type : "ack",
+        data : id
+      });
+      websocket.send(ack);
+  };
+
+  this.send = function(str) {
+    console.log("sending: " + str);
+    websocket.send(str);
+  };
+
+  this.close = function() {
+      websocket.close();
+  };
+
+}
+
+/**************************************************************************
+* Objects
+***************************************************************************/
+
+// var conn = new Connection(ADDRESS);
+
+/**************************************************************************
+ * Functions
+ **************************************************************************/
+
+/* 
+ * On the server, (X,Y) positions refer to the center of objects.
+ * Given a X position on a 0 to <INTERNAL_DIM> scale, and the width
+ * of the object on the screen, we can derive where its left-most
+ * position should be on the screen.
+ */
+function convertXPos(x, width) {
+  var xpos = x * K;
+  xpos = xpos - (width / 2);
+  return xpos;
+}
+
+/* Same as above but with Y position and height */
+function convertYPos(y, height){
+  var ypos = DIMENSIONS.h - (y * K);
+  ypos = ypos - (height / 2);
+  return ypos;
+}
+
+/***************************************************************************
+ * Capture keydown & keyup events and send to the server
+ **************************************************************************/
+
+// var onkey = function(evt) {
+//   var cmd = {
+//     type : evt.type,
+//     data : evt.keyCode
+//   };
+//   return conn.send(JSON.stringify(cmd));
+// };
+
+// var body = document.getElementById("body");
+// body.onkeydown = onkey;
+// body.onkeyup = onkey;
+
+/***************************************************************************
+ * Send a "quit" signal to the server just before the page unloads
+ **************************************************************************/
+// window.onbeforeunload = function() {
+//   onkey({
+//     type : "keydown",
+//     keyCode : 81
+//   });
+// };
